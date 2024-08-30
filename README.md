d56d1c76e4a22ef075108c2eaa2cefbd9add8012
Estrella-Explore <Estrella_Explore@outlook.com>
2024-08-30 10:01:25 +0800

    Inital change

---
+++ README.md
@@ -0,0 +1,11 @@
+# Description
+
+> To enable the **STRICTEST compiler flags** for OIers (single file compile script).
+
+# How to use
+
+```bash
+sh ./make.sh ./demo/foo.cpp
+```
+
+The `a.out` file is in the same dir of foo.cpp.
---
+++ make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -g -Wall -Wextra -pedantic --std=c++14 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fdiagnostics-color=always $1
