5cbbba343fb70aa33a5e708953b49e9366dbcb9b
Estrella-Explore <Estrella_Explore@outlook.com>
2024-10-11 16:49:32 +0800

    chore(make.sh): add colorful output.
    Add colorful output in function `tryUsingDefaultTestcase()`.

--- make.sh
+++ make.sh
@@ -19,30 +19,6 @@ fi
 # E.g.: Your file name is `foo.cpp`, then your executable file name is `foo.out`.
 g++ -g -Wall -Wextra -pedantic --std=c++14 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fdiagnostics-color=always $1 -o ${filename}.out
 
-# @brief: read default test as `stdin`, redirect `stdout` to `${filename}.ans` .
-# If found `${filename}.in`, \
-# ask user if they want to use `${filename}.in` as testcase. \
-# Output was been redirected to `${filename}.ans`
-tryUsingDefaultTestcase() {
-    if test -r ${filename}.in; then # Test if `${filename}.in` exists and readable.
-        echo ""
-        echo "[Info]: Testcase ${filename}.in was detected."
-        echo "------> Using this testcase? [Y/n]"
-        read -r operation
-
-        if [[ "$operation" != [Nn]* ]]; then                   # If `$operation` is not "N" or "n", include empty input.
-            ./${filename}.out <${filename}.in >${filename}.ans # Run program.
-
-            echo "[Info]: Reading ${filename}.in as testcase."
-            echo -e "[Info]: Your answer is below, which will be saved as ${filename}.ans.\n"
-
-            cat ${filename}.ans
-
-            echo -e "\n[Hint]: You can try \"diff ${filename}.ans <Standard Answer>\" to debug."
-        fi
-    fi
-}
-
 # @brief: Receive a string as parameter, output it in blue.
 # `\033[34m` sets the color to blue, \
 # `\033[0m` clear all the colors
@@ -57,6 +33,30 @@ redOutput () {
     echo -e "\033[31m${1}\033[0m"
 }
 
+# @brief: read default test as `stdin`, redirect `stdout` to `${filename}.ans` .
+# If found `${filename}.in`, \
+# ask user if they want to use `${filename}.in` as testcase. \
+# Output was been redirected to `${filename}.ans`
+tryUsingDefaultTestcase() {
+    if test -r ${filename}.in; then # Test if `${filename}.in` exists and readable.
+        echo ""
+        blueOutput "[Info]:\033[0m Testcase ${filename}.in was detected."
+        echo "------> Using this testcase? [Y/n]"
+        read -r operation
+
+        if [[ "$operation" != [Nn]* ]]; then                   # If `$operation` is not "N" or "n", include empty input.
+            ./${filename}.out <${filename}.in >${filename}.ans # Run program.
+
+            blueOutput "[Info]:\033[0m Reading ${filename}.in as testcase."
+            blueOutput "[Info]:\033[0m Your answer is below, which will be saved as ${filename}.ans.\n"
+
+            cat ${filename}.ans
+
+            blueOutput "\n[Hint]:\033[0m You can try \"diff ${filename}.ans <Standard Answer>\" to debug."
+        fi
+    fi
+}
+
 # ---------- Functions definitions finished ----------
 
 # if [[ $? == 0 ]]; then
@@ -67,6 +67,6 @@ if test -x ${filename}.out; then # If `${filename}.out` exists and can be execut
     tryUsingDefaultTestcase
 else
     # TODO: Output the above compile informations to `${filename}.log`
-    redOutput "[Error]: Got trouble in compiling..."
+    redOutput "[Error]: Got trouble in compiling...\a"
     blueOutput "[Info]:\033[0m Exiting..."
 fi
